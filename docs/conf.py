559777edc7e9a51ff5887038c1b28690589f4ced
coding-runoob <52923525+coding-runoob@users.noreply.github.com>
2024-05-11 03:42:00 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,5 +30,5 @@ language = 'zh'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+
 html_static_path = ['_static']
