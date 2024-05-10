670a7134c128b1944fb201e8be6789db7473199d
coding-runoob <52923525+coding-runoob@users.noreply.github.com>
2024-05-11 03:03:49 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ author = 'yang'
 
 extensions = []
 # import sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # from recommonmark.parser import CommonMarkParser
 # source_parsers = {
